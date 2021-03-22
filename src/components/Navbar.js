6f847eeebc38652a50cd2863f0266ac5b58a14d2
Kriszti Toth <k95toth@gmail.com>
2021-03-22 14:59:15 +0100

    feature: implement navbar with basic design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const NavBar = styled.div`
+  overflow: hidden;
+  background-color: #333;
+  position: fixed;
+  top: 0;
+  width: 100%;
+`;
+
+const NavLink = styled(Link)`
+  float: right;
+  display: block;
+  color: white;
+  font-weight: bold;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+
+  :hover {
+    color: #ad6242;
+  }
+`;
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <NavBar>
+      <NavLink to="/favorites">Favorites</NavLink>
+      <NavLink to="/photos">Photos</NavLink>
+      <NavLink to="/rovers">Rovers</NavLink>
+      <NavLink to="/">Home</NavLink>
+    </NavBar>
   );
 };
 
