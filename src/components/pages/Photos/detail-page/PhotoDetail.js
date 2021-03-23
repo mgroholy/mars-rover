605166e93af966aa6920530d4da3ba519c4d8d39
Kriszti Toth <k95toth@gmail.com>
2021-03-23 14:09:38 +0100

    feature: implement styled PhotoDetail component

--- src/components/pages/Photos/detail-page/PhotoDetail.js
+++ src/components/pages/Photos/detail-page/PhotoDetail.js
@@ -1,16 +1,40 @@
 import React from "react";
+import styled from "styled-components";
+
+const Card = styled.div`
+  margin: 0 auto;
+  margin-top: 100px;
+  margin-bottom: 50px;
+  width: 800px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  background: #ad6242;
+  border-radius: 5px;
+`;
+
+const Description = styled.div`
+  padding: 10px 15px 15px 15px;
+  font-weight: bold;
+`;
+
+const Image = styled.img`
+  border-radius: 5px 5px 0 0;
+  width: 800px;
+`;
 
 const PhotoDetail = (props) => {
-  const { id } = props.match.params;
   const { image, rover, date } = props.location.state;
 
   return (
-    <div style={{ marginTop: "100px" }}>
-      <div>{id}</div>
-      <div>{image}</div>
-      <div>{rover}</div>
-      <div>{date}</div>
-    </div>
+    <Card>
+      <Image alt="mars-quality" src={image} />
+      <Description>
+        <div style={{ display: "inline-block", marginRight: "420px" }}>
+          Rover: {rover}
+        </div>
+        <div style={{ display: "inline-block" }}>Earth date: {date}</div>
+      </Description>
+    </Card>
   );
 };
 
