07832dc5f7956a8bba1877d9c2ef930b874891f4
Kriszti Toth <k95toth@gmail.com>
2021-03-23 14:35:41 +0100

    feature: add Go back button to detail page

--- src/components/pages/Photos/detail-page/PhotoDetail.js
+++ src/components/pages/Photos/detail-page/PhotoDetail.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 
-const Card = styled.div`
+const Container = styled.div`
   margin: 0 auto;
-  margin-top: 100px;
-  margin-bottom: 50px;
+  margin-top: 80px;
+  margin-bottom: 25px;
   width: 800px;
+`;
+
+const Card = styled.div`
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   transition: 0.3s;
   background: #ad6242;
   border-radius: 5px;
+  margin-bottom: 25px;
 `;
 
 const Description = styled.div`
@@ -22,19 +27,34 @@ const Image = styled.img`
   width: 800px;
 `;
 
+const Button = styled.button`
+  background-color: #333;
+  color: white;
+  font-weight: bold;
+  text-decoration: none;
+  padding: 8px 16px;
+
+  :hover {
+    color: #ad6242;
+  }
+`;
+
 const PhotoDetail = (props) => {
   const { image, rover, date } = props.location.state;
 
   return (
-    <Card>
-      <Image alt="mars-quality" src={image} />
-      <Description>
-        <div style={{ display: "inline-block", marginRight: "420px" }}>
-          Rover: {rover}
-        </div>
-        <div style={{ display: "inline-block" }}>Earth date: {date}</div>
-      </Description>
-    </Card>
+    <Container>
+      <Card>
+        <Image alt="mars-quality" src={image} />
+        <Description>
+          <div style={{ display: "inline-block", marginRight: "420px" }}>
+            Rover: {rover}
+          </div>
+          <div style={{ display: "inline-block" }}>Earth date: {date}</div>
+        </Description>
+      </Card>
+      <Button onClick={useHistory().goBack}>&lt; Go back</Button>
+    </Container>
   );
 };
 
