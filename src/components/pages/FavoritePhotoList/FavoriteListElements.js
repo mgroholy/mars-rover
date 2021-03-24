0ba8466ebd2cf8cae474ba2965998ba9f1bedd3c
Máté Groholy <mate.groholy@gmail.com>
2021-03-24 13:18:41 +0100

    feature: rearrange FavoritePhotoList elements. Add pagination.

--- src/components/pages/FavoritePhotoList/FavoriteListElements.js
+++ src/components/pages/FavoritePhotoList/FavoriteListElements.js
@@ -17,17 +17,19 @@ export const FavoriteCard = styled.div`
   overflow: hidden;
   background: #fff;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
+  flex-direction: row;
+  justify-content: space-between;
   align-items: center;
+  box-shadow: 0 3rem 6rem rgba(0, 0, 0, 0.1), 1px 5px 20px rgba(0, 0, 0, 0.2);
 `;
 
 export const FavoriteHeroWrapper = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
   width: 100%;
   height: 70%;
+  /* background: blue; */
 `;
 
 export const FavoriteHeroImage = styled(Link)`
@@ -44,11 +46,19 @@ export const FavoriteHeroImage = styled(Link)`
   }
 `;
 
+export const HeroImageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 70%;
+  height: 100%;
+`;
+
 export const FavoriteSelection = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 28%;
+  height: 100%;
   width: 100%;
 `;
 
@@ -57,9 +67,16 @@ export const FavoriteSelectionCard = styled.img`
   width: 150px;
   border-radius: 5px;
   margin: 20px;
+  /* background: grey; */
   cursor: pointer;
+  /* border: ${({ selected }) =>
+    selected === "true" ? "5px solid #ad6242" : "none"}; */
+  transform: ${({ selected }) =>
+    selected === "true" ? "scale(1.15)" : "none"};
+
   &:hover {
-    transform: scale(1.05);
+    /* transform: scale(1.05); */
+    opacity: 0.7;
     transition: 0.3s ease-in-out;
   }
 `;
@@ -67,5 +84,59 @@ export const FavoriteSelectionCard = styled.img`
 export const SelectionDivider = styled.div`
   width: 80%;
   height: 10px;
-  border-bottom: 2px solid #333;
+  border-bottom: 3px solid #ad6242;
+`;
+
+export const FavoriteSidebar = styled.div`
+  height: 100%;
+  width: 40%;
+  display: flex;
+  flex-direction: column;
+  /* background: green; */
+`;
+
+export const FavoriteGallery = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  /* background: red; */
+`;
+
+export const SidebarTitle = styled.h1`
+  font-size: 42px;
+  width: 100%;
+  margin-left: 20px;
+  margin-top: 20px;
+  margin-bottom: 10px;
+`;
+
+export const SidebarDescription = styled.div`
+  margin-left: 20px;
+  margin-top: 10px;
+  line-height: 2.5rem;
+`;
+
+export const FavoritePhotoDetails = styled.div`
+  width: 100%;
+  margin-left: 40px;
+  font-size: 18px;
+`;
+
+export const Arrow = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  width: 10%;
+`;
+
+export const FavoriteSelectionWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 28%;
+  width: 100%;
 `;
--- src/components/pages/FavoritePhotoList/FavoritePhotoList.js
+++ src/components/pages/FavoritePhotoList/FavoritePhotoList.js
@@ -1,54 +1,119 @@
 import React, { useContext, useEffect, useState } from "react";
 import FavoriteContext from "../../context/FavoriteContext";
-import PhotoItem from "../Photos/list-page/PhotoItem";
 import {
   FavoriteCard,
   FavoriteContainer,
+  FavoriteGallery,
   FavoriteHeroImage,
   FavoriteHeroWrapper,
   FavoriteSelection,
   FavoriteSelectionCard,
+  FavoriteSelectionWrapper,
+  FavoriteSidebar,
+  HeroImageWrapper,
+  Arrow,
   SelectionDivider,
+  SidebarDescription,
+  SidebarTitle,
 } from "./FavoriteListElements";
+import { BiLeftArrow, BiRightArrow } from "react-icons/bi";
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 const FavoritePhotoList = () => {
   const [favorites] = useContext(FavoriteContext);
   const [heroImg, setHeroImg] = useState({ image: "" });
 
-  const selectHeroImage = (src) => {
-    setHeroImg(src);
+  const [displayedPhotos, setDisplayedPhotos] = useState([]);
+  const [numberOfDisplayedPhotos, setNumberOfDisplayedPhotos] = useState(7);
+  const [firstDisplayed, setFirstDisplayed] = useState(0);
+  const [lastDisplayed, setLastDisplayed] = useState(
+    firstDisplayed + numberOfDisplayedPhotos
+  );
+
+  const nextPage = () => {
+    setFirstDisplayed(firstDisplayed + numberOfDisplayedPhotos);
+    setLastDisplayed(lastDisplayed + numberOfDisplayedPhotos);
+    setDisplayedPhotos(favorites.slice(firstDisplayed, lastDisplayed));
+  };
+
+  const previousPage = () => {
+    setFirstDisplayed(firstDisplayed - numberOfDisplayedPhotos);
+    setLastDisplayed(lastDisplayed - numberOfDisplayedPhotos);
+    setDisplayedPhotos(favorites.slice(firstDisplayed, lastDisplayed));
   };
 
   useEffect(() => {
     if (favorites.length > 0) {
       setHeroImg(favorites[0]);
+      setDisplayedPhotos(favorites.slice(firstDisplayed, lastDisplayed));
     }
-  }, [favorites]);
+  }, [favorites, firstDisplayed, lastDisplayed]);
+
+  // window.addEventListener("resize", () => {
+  //   if (window.innerWidth <= 1300) {
+  //     setNumberOfDisplayedPhotos(3);
+  //   } else if (window.innerWidth <= 1600) {
+  //     setNumberOfDisplayedPhotos(5);
+  //   } else {
+  //     setNumberOfDisplayedPhotos(7);
+  //   }
+  // });
 
   return (
     <div>
       <FavoriteContainer>
         <FavoriteCard>
-          <FavoriteHeroWrapper>
-            <FavoriteHeroImage
-              to={{
-                pathname: `/photo/${heroImg.id}`,
-                state: heroImg,
-              }}
-            >
-              <img src={heroImg.image} alt="mars" />
-            </FavoriteHeroImage>
-          </FavoriteHeroWrapper>
-          <SelectionDivider />
-          <FavoriteSelection>
-            {favorites.map((favorite) => (
-              <FavoriteSelectionCard
-                src={favorite.image}
-                alt="mars"
-                onClick={() => setHeroImg(favorite)}
-              />
-            ))}
-          </FavoriteSelection>
+          <FavoriteGallery>
+            <FavoriteHeroWrapper>
+              <HeroImageWrapper>
+                <FavoriteHeroImage
+                  to={{
+                    pathname: `/photo/${heroImg.id}`,
+                    state: heroImg,
+                  }}
+                >
+                  <img src={heroImg.image} alt="mars" />
+                </FavoriteHeroImage>
+              </HeroImageWrapper>
+              <FavoriteSidebar>
+                <SidebarTitle>Favorite Photos</SidebarTitle>
+                <SelectionDivider />
+                <SidebarDescription>
+                  <h2>Photo details:</h2>
+                  <p>Rover: {heroImg.rover}</p>
+                  <p>Date: {heroImg.date}</p>
+                  <p>Id: {heroImg.id}</p>
+                </SidebarDescription>
+              </FavoriteSidebar>
+            </FavoriteHeroWrapper>
+
+            <SelectionDivider />
+            <FavoriteSelectionWrapper>
+              {console.log("firstdisplayed", firstDisplayed)}
+              <Arrow>
+                {firstDisplayed > 0 && (
+                  <IoIosArrowBack size={40} onClick={previousPage} />
+                )}
+              </Arrow>
+              <FavoriteSelection>
+                {console.log(displayedPhotos)}
+                {displayedPhotos.map((favorite) => (
+                  <FavoriteSelectionCard
+                    src={favorite.image}
+                    alt="mars"
+                    onClick={() => setHeroImg(favorite)}
+                    selected={favorite.id === heroImg.id ? "true" : "false"}
+                  />
+                ))}
+              </FavoriteSelection>
+              {console.log("lastDisplayed", lastDisplayed)}
+              <Arrow>
+                {lastDisplayed < favorites.length && (
+                  <IoIosArrowForward size={40} onClick={nextPage} />
+                )}
+              </Arrow>
+            </FavoriteSelectionWrapper>
+          </FavoriteGallery>
         </FavoriteCard>
       </FavoriteContainer>
     </div>
