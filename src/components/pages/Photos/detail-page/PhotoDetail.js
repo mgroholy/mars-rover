58d629b9751be2cce6d1361dbe46ae99628e84ac
Kriszti Toth <k95toth@gmail.com>
2021-03-24 18:19:01 +0100

    feature: resize image dynamically to fit the window on PhotoDetail page

--- src/components/pages/Photos/detail-page/PhotoDetail.js
+++ src/components/pages/Photos/detail-page/PhotoDetail.js
@@ -6,17 +6,14 @@ import FavoriteContext from "../../../context/FavoriteContext";
 
 const Container = styled.div`
   margin: 0 auto;
-  margin-top: 80px;
+  margin-top: 25px;
   margin-bottom: 25px;
   width: 800px;
 `;
 
 const Card = styled.div`
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  transition: 0.3s;
-  background: #ad6242;
-  border-radius: 5px;
-  margin-bottom: 25px;
+  display: grid;
+  height: 100%;
 `;
 
 const Description = styled.div`
@@ -25,11 +22,17 @@ const Description = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  background: #ad6242;
+  border-radius: 5px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  transition: 0.3s;
+  background: #ad6242;
 `;
 
 const Image = styled.img`
-  border-radius: 5px 5px 0 0;
-  width: 800px;
+  max-width: 100%;
+  max-height: 100vh;
+  margin: auto;
 `;
 
 const Button = styled.button`
@@ -38,6 +41,7 @@ const Button = styled.button`
   font-weight: bold;
   text-decoration: none;
   padding: 8px 16px;
+  margin-top: 25px;
 
   :hover {
     color: #ad6242;
@@ -85,9 +89,9 @@ const PhotoDetail = (props) => {
   }, [favorited, id, favorites]);
 
   return (
-    <Container>
-      <Card>
-        <Image alt="mars-quality" src={image} />
+    <Card>
+      <Image alt="mars-quality" src={image} />
+      <Container>
         <Description>
           <div style={{ display: "inline-block" }}>Rover: {rover}</div>
           <FavoriteContainer
@@ -102,9 +106,9 @@ const PhotoDetail = (props) => {
           </FavoriteContainer>
           <div style={{ display: "inline-block" }}>Earth date: {date}</div>
         </Description>
-      </Card>
-      <Button onClick={useHistory().goBack}>&lt; Go back</Button>
-    </Container>
+        <Button onClick={useHistory().goBack}>&lt; Go back</Button>
+      </Container>
+    </Card>
   );
 };
 
