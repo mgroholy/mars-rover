7cf4aefd4e49ccf3f1239aefe2f15e56386249f9
Kriszti Toth <k95toth@gmail.com>
2021-03-25 10:19:17 +0100

    refactor: move styled photo detail components to separate file

--- src/components/pages/PhotoDetail/PhotoDetail.js
+++ src/components/pages/PhotoDetail/PhotoDetail.js
@@ -1,64 +1,16 @@
 import React, { useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-import styled from "styled-components";
+import {
+  Container,
+  Card,
+  Description,
+  Image,
+  Button,
+  FavoriteContainer,
+  FavoriteIcon,
+} from "./PhotoDetailElements";
 import { FaRegStar, FaStar, FaRegTrashAlt, FaTrashAlt } from "react-icons/fa";
 import FavoriteContext from "../../context/FavoriteContext";
-
-const Container = styled.div`
-  margin: 0 auto;
-  margin-top: 25px;
-  margin-bottom: 25px;
-  width: 800px;
-`;
-
-const Card = styled.div`
-  display: grid;
-  margin-top: 58px; // Navbar height + 10px (upper frame)
-`;
-
-const Description = styled.div`
-  padding: 10px 15px 15px 15px;
-  font-weight: bold;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background: #ad6242;
-  border-radius: 5px;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  transition: 0.3s;
-  background: #ad6242;
-`;
-
-const Image = styled.img`
-  max-width: 100%;
-  max-height: calc(100vh - 68px); // Navbar height + 20px (bottom frame)
-  border: 10px #333 solid;
-  margin: auto;
-`;
-
-const Button = styled.button`
-  background-color: #333;
-  color: white;
-  font-weight: bold;
-  text-decoration: none;
-  padding: 8px 16px;
-  margin-top: 25px;
-
-  :hover {
-    color: #ad6242;
-  }
-`;
-
-const FavoriteContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-`;
-
-const FavoriteIcon = styled.span`
-  margin-right: 10px;
-`;
 
 const PhotoDetail = (props) => {
   const { id, image, rover, date } = props.location.state;
