c4456ad61ac3fc2639c1daf3dce1e4d309cff14f
Kriszti Toth <k95toth@gmail.com>
2021-03-27 12:41:10 +0100

    feature: implement MobileFilterbar component without functionality

--- src/components/pages/PhotoList/Filterbar/MobileFilterbar.js
+++ src/components/pages/PhotoList/Filterbar/MobileFilterbar.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { Container, FilterOptions, RoverLink } from "./MobileFilterbarElements";
 
 export const MobileFilterbar = ({ isOpen, toggleMobileFilter }) => {
-  return <div></div>;
+  return (
+    <Container isOpen={isOpen}>
+      <FilterOptions>
+        <RoverLink exact to="#" onClick={toggleMobileFilter}>
+          Curiosity
+        </RoverLink>
+        <RoverLink to="#" onClick={toggleMobileFilter}>
+          Opportunity
+        </RoverLink>
+        <RoverLink to="#" onClick={toggleMobileFilter}>
+          Spirit
+        </RoverLink>
+        <RoverLink to="#" onClick={toggleMobileFilter}>
+          Perseverance
+        </RoverLink>
+      </FilterOptions>
+    </Container>
+  );
 };
--- src/components/pages/PhotoList/Filterbar/MobileFilterbarElements.js
+++ src/components/pages/PhotoList/Filterbar/MobileFilterbarElements.js
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 export const MobileIcon = styled.div`
@@ -19,4 +20,45 @@ export const MobileText = styled.div`
   font-weight: bolder;
   color: #333;
   margin-left: 10px;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  position: absolute;
+  left: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
+  top: 150px;
+  background: lightgray;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 50%;
+  z-index: 10;
+  border-radius: 5px;
+
+  transition: left 0.3s ease-in-out;
+`;
+
+export const FilterOptions = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+export const RoverLink = styled(NavLink)`
+  text-decoration: none;
+  font-weight: bolder;
+  color: #333;
+  margin: 20px;
+  width: 60%;
+  padding-bottom: 5px;
+  text-align: center;
+  border-bottom: 2px solid #ad6242;
+
+  :hover {
+    color: #ad6242;
+  }
 `;
